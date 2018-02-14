3535d8ce9197c717d76136142a7a6b1679fa078f
ef718 <elfang16@gmail.com>
2018-02-14 03:01:49 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(function() {
+    var symbols = ['%', '%', '@', '@', '$', '$', '#', '#']
+
+
+    function Card(div, symbol) {
+        var symbol = "%";
+    }
+
+
+    $( ".flip-card" ).each(function(index, div) {
+
+    });
+
+    $( ".flip-card" ).click(function() {
+        $(this).css('background-color', 'white');
+    });
+});
