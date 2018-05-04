085ed199ce4c5f8e04921800f40cdfcf38c05c53
ef718 <elfang16@gmail.com>
2018-05-04 01:49:35 -0400

    Set up timer

--- js/main.js
+++ js/main.js
@@ -1,64 +1,35 @@
 $(function() {
-    var symbols = ['%', '%', '@', '@', '$', '$', '#', '#']; // TODO: shuffle this array
+    var $light = $('.light');
 
-
-    function Card(div, symbol) {
-        this.symbol = symbol;
-        this.facing_up = false;
-
-        this.flip = function() {
-            if (this.facing_up) {
-                //flip the div face down
-                this.facing_up = false;
-                $(this).css('background-color', 'cornflowerblue');
-            } else {
-                //flip the div face up
-                this.facing_up = true;
-                $(div).css('background-color', 'white');
-                $(div).text(symbol);
-            }
-        }
-    }
-
-    function shuffleArray(array) {
-        for (var i = array.length - 1; i > 0; i--) {
-            var j = Math.floor(Math.random() * (i + 1));
-            var temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
-
-
-    var tested_card = null;
-
-    $(".flip-card").each(function(index, div) {
-        var card = new Card(div, symbols[index]);
-
-        $(div).click(function() {
-            if (card.facing_up) {
-                //TODO: people probably shouldn't click faced up cards?
-            } else {
-                card.flip();
-
-                if (tested_card === null) {
-                    tested_card = card;
-                } else {
-                    if (tested_card.symbol === card.symbol) {
-                        //leave them both up
-                        tested_card = null;
-                    } else {
-                        //TODO: sleep for like 3 seconds
-                        tested_card.flip();
-                        card.flip();
-                        tested_card = null;
-                    }
-                }
-            }
-        });
+    $(document).on('click', '.start-timer', function() {
+        $light.css('background-color', 'lightskyblue');
+        var buttonClicked = $(this).text()
+        var minutesArray = buttonClicked.split(' ')[0].split('-');
+        var greenMs = Number(minutesArray[0])*60*1000;
+        var redMs = Number(minutesArray[1])*60*1000;
+        var yellowMs = (greenMs + redMs) / 2;
+        $('h1').text('Speech Timer: ' + buttonClicked);
+        startTimer(greenMs, yellowMs, redMs);
     });
 
-    // $( ".flip-card" ).click(function() {
-    //     $(this).css('background-color', 'white');
-    // });
+    $(document).on('click', '.stop-timer', function() {
+        location.reload();
+    });
+
+    function startTimer(greenMilliseconds, yellowMilliseconds, redMilliseconds) {
+        // greenMilliseconds = 2000;
+        // yellowMilliseconds = 4000;
+        // redMilliseconds = 6000;
+        changeLight('green', greenMilliseconds);
+        changeLight('yellow', yellowMilliseconds);
+        changeLight('crimson', redMilliseconds);
+    }
+
+    function changeLight(color, milliseconds) {
+        setTimeout(function() {
+            $light.css('background-color', color);
+            var timeRemaining = (milliseconds / 60000).toFixed(1);
+            $('.time-remaining').html('Time Reached:' + '<br />' + timeRemaining + ' minutes');
+        }, milliseconds);
+    }
 });
